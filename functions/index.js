fef7cb9157f092f8d462f26433e8e1d4e016fb0f
shomy032 <milosmilic032@gmail.com>
2022-07-12 11:51:48 +0200

    added first valid cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,17 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+//     const original = event.data.val();
+
+exports.addCreationDateToPost = functions.firestore
+.document("/app/{instance}/posts/{userId}")
+.onCreate((snapshot, context) => {
+    return snapshot.ref.update({dateOfCreation: Date.now(),
+     likes: 0,
+    userLiked: []});
 });
